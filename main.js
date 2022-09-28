38d37f12cf9be45b78ee9e4b6dfbc5c16d880c3f
Juan Manuel Viera <juanvieracespedes@gmail.com>
2022-09-27 23:51:58 -0300

    creando menu con suma de precios

--- main.js
+++ main.js
@@ -2,13 +2,41 @@ let usuario= prompt("Ingrese su usuario");
 let contraseña= prompt("Ingrese Contraseña");
 
 if(usuario =="Admin" && contraseña=="1234"){
-    alert("Bienvenido a Pulperia de Beto&Clau. Haga su pedido!")
-    let pedido= prompt("Que vas a pedir? Para finalizar, ingresa 0")
+    alert("Bienvenido a Pulperia de Beto&Clau. Haga su pedido!");
+    let pedido= prompt("Que vas a pedir? \n 1- Asado $400 \n 2- Pulpon $400 \n 3- Papas Fritas $150 \n 4- Coca-Cola $80 \n 5- Agua $60 \n 0- Finalizar pedido");
+    switch(pedido){
+        case "1":
+            console.log("Añadiste Asado");
+            let Asado= ParseInt("400");
+        break;
+        case "2":
+            console.log("Añadiste Pulpon");
+            let Pulpon= ParseInt("400");
+        break;
+        case "3":
+            console.log("Añadiste Papas Fritas");
+            let PFritas= ParseInt("150");
+        break;
+        case "4":
+            console.log("Añadiste Coca-Cola");
+            let Coca= ParseInt("80");
+        break;
+        case "5":
+            console.log("Añadiste Agua");
+            let Agua= ParseInt("60");
+        break;
+            
+    }
 
 while (pedido != 0) {
-    alert("Ingresaste " + pedido)
-    pedido= prompt("Algo mas?")
+    alert("Ingresaste " + pedido);
+    pedido= prompt("Algo mas?");
     
+}
+
+if(pedido== "0"){
+    let miCuenta= ParseInt(pedido + pedido)
+    alert("El total a pagar es " + miCuenta )
 }
 }
 else{
